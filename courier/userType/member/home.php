701b3deba5aad62bd6f087958726ef05c840d475
EKSViLiZ <155067823+EKSViLiZ@users.noreply.github.com>
2023-12-29 12:54:00 +0800

    Add files via upload

---
+++ courier/userType/member/home.php
@@ -0,0 +1,20 @@
+<?php
+    /*Checks if User is already Logged In*/
+    session_start();
+    
+    if (!isset($_SESSION["user"])) {
+        header("Location: ../guest/home.php");
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courier - Home Page</title>
+</head>
+<body>
+    <?php echo "Hello ".$_SESSION["user"]?><a href="../../registry/logout.php"><button class="btn logoutBtn">Logout</button></a>
+</body>
+</html>
