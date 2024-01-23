51168ba1312b230e92a68521af292ca7426d8cb7
EKSViLiZ <155067823+EKSViLiZ@users.noreply.github.com>
2024-01-23 18:13:11 +0800

    Add files via upload

---
+++ courier/create/newDiscussion.php
@@ -0,0 +1,103 @@
+<?php
+    /*Checks if User is already Logged In*/
+    session_start();
+    
+    if (isset($_SESSION["user"])) {
+        header("Location: ../member/home.php");
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start a New Discussion!</title>
+    
+    <link rel="stylesheet" href="newDiscussion.css">
+</head>
+<body>
+    <div class="topPanel">
+        <div class="logo">
+            <a href="../userType/guest/home.php" title="Go to home page"><img src="../media/logo/courier_logo_primary_alt.png" alt="Courier Logo" style="width: 120px"></a>
+        </div>
+        
+        <div class="userProfile">
+            <label class="guestStatus">Viewing as Guest</label>
+            
+                
+            <div class="registry">
+                <div class="signup">
+                    <a href="../registry/start.php"><button class="registerBtn">Sign Up</button></a>
+                </div>
+                
+                <div class="login">
+                    <a href="../registry/start.php"><button class="registerBtn">Login</button></a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="searchBarContainer">
+            <input class="searchBar" type="text" name="searchBar" placeholder="Search">
+        </div>
+        
+    </div>
+    
+    <div class="sidePanelLeft">
+
+        <div class="navigationMenu">
+            <a href="../userType/guest/home.php">
+                <div class="navigationBtn navigationUnselected">
+                    Home
+                </div>
+            </a>
+            
+            <a href="followed.php">
+                <div class="navigationBtn navigationUnselected">
+                    Followed
+                </div>
+            </a>
+            
+            <a href="visited.php">
+                <div class="navigationBtn navigationUnselected">
+                    Visited
+                </div>
+            </a>
+            
+            <a href="popular.php">
+                <div class="navigationBtn navigationUnselected">
+                    Popular
+                </div>
+            </a>
+            
+            <a href="recent.php">
+                <div class="navigationBtn navigationUnselected">
+                    Recent
+                </div>
+            </a>
+        </div>
+        
+    </div>
+    
+    <div class="sidePanelRight">
+        <div class="containerSecondary">
+            <label class="navigationBtn relevantBoards">Relevant Boards</label>
+        </div>
+        
+        <div class="createPostContainer">
+            <a href="newDiscussion.php"><button class="createBtn">Start a Discussion</button></a>
+            
+            <p style="text-align: center;">OR</p>
+            
+            <a href="newThread.php"><button class="createBtn">Create New Thread</button></a>
+        </div>
+    </div>
+    
+    <form action="newDiscussion.php" class="container" method="post">  
+        
+    </form>
+    
+    <div class="footer">
+    </div>
+</body>
+</html>
---
+++ courier/registry/login.php
@@ -0,0 +1,88 @@
+<?php 
+    /*Checks if User is already Logged In*/
+    session_start();
+    
+    if (isset($_SESSION["user"])) {
+        header("Location: ../userType/member/home.php");
+    }
+    
+    require_once "../index/index.php";
+    
+    if (isset($_POST["login"])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        
+        $sql = "SELECT * FROM user WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        
+        if (empty($email) OR (empty($password))) {
+            echo "<div class='alert alertError alertPrimaryCenter'>All fields are required!</div>";
+        } else if ($user){
+            if (password_verify($password, $user["password"])) {
+                session_start();
+                $_SESSION["user"] = $user["username"];
+                header("Location: login.php");
+                die();
+            } else {
+                echo "<div class='alert alertError alertPrimaryCenter'>Password does not match!</div>";
+            }
+        } else {
+            echo "<div class='alert alertError alertPrimaryCenter'>Email does not match!</div>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courier - Login Page</title>
+
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="topPanel"></div>
+    
+    <div class="blankContainer">
+        <form class="container" action="login.php" method="post">
+            <div class="logo">
+                <a href="../userType/guest/home.php" title="Go to home page"><img src="../media/logo/courier_logo_primary.png" alt="Courier Logo" style="width: 185px"></a>
+            </div>
+            
+            <p class="header1">Login</p>
+            
+            <label class="header2" for="email">Email Address:</label><br>
+            <input class="containerInput" type="email" id="email" name="email"><br>
+
+            <label class="header2" for="password">Password:</label><br>
+            <input class="containerInput" type="password" id="password" name="password"><br>
+            
+            <div class="showPassword">
+                <input type="checkbox" onclick="showPassword()">Show Password<br>
+            </div>
+
+            <input  class="btn loginBtn" type="submit" value="Login" name="login">
+
+            <div class="register">
+                Don't have an account? <a href="signup.php"><u>Register Now!</u></a>
+            </div>
+        </form>
+
+        <button onclick="history.go(-1);" class="btn backBtn">Back </button>
+    </div>
+    
+    <script>
+        function showPassword() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+        }
+    </script>
+
+</body>
+</html>
